c13ffcc1581e80a0ac945007ebd47838624ce656
MalcolmMacDonald <mac97@shaw.ca>
2026-03-03 19:54:22 -0800

    changed url path

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig(() => ({
     plugins: [react()],
-    base: command === 'serve' ? '/' : '/cursed-apple-guesser/',
+    base: '/',
     publicDir: 'public',
     assetsInclude: ['**/*.png']
 }))
