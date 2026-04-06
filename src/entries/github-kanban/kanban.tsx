24db8d42403d5bcdf6409e8baef81fe01d47c0b5
MalcolmMacDonald <mac97@shaw.ca>
2026-04-06 12:40:22 -0700

    Another attempted fix for github kanban

--- src/entries/github-kanban/kanban.tsx
+++ src/entries/github-kanban/kanban.tsx
@@ -2,7 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import '@/index.css';
 import PageShell from '@/components/PageShell';
-import {GithubKanban} from '@malcolm/github-kanban';
+import {GithubKanban} from '@malcolmmacdonald/github-kanban';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
